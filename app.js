324b09490121267ad269c3bf676c18152bc79c80
greedy854 <114736063+greedy854@users.noreply.github.com>
2022-12-19 14:27:10 +0100

    les 5

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ class App
 {
     runApplication()
     {
-        console.log("hello world")
+        console.log("hello world!")
+        let arr = ["kanye west", "hasan", "ken"]
+        headers[0].innerhtml = "Lennie Briscoe";
+        headers[1].innerHTML = arr[1];
+        headers[2].innerHTML = arr[2];
+        headers[3].innerHTML = arr[3];
+
+        for(let i = o; i <headers.length; i++){
+            headers[i].innerHTML = arr[i]
+        }
+        console.log()
     }
 }
-
 let app = new App();
 app.runApplication();
 
-let eenVariablenaam = waarde
 
-let waarde = new App
-app.runApplication(waarde);
+
+
+
+
