a09c55d765903971340a460128ea69405e992fe8
greedy854 <114736063+greedy854@users.noreply.github.com>
2022-12-20 11:46:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,68 @@
-class App
-{
-    runApplication()
-    {
-        console.log("hello world!")
-        let arr = ["kanye west", "hasan", "ken"]
-        headers[0].innerhtml = "Lennie Briscoe";
-        headers[1].innerHTML = arr[1];
-        headers[2].innerHTML = arr[2];
-        headers[3].innerHTML = arr[3];
+class App{
+    runApplication() {
 
-        for(let i = o; i <headers.length; i++){
-            headers[i].innerHTML = arr[i]
-        }
-        console.log()
+
+    let button = document.getElementById("rerollButton")
+    button.addEventListener('click' , ()=>{
+        console.log("hello world");
+        console.log("hello world!");
+        let stat1 = this.cijfertellen();
+        let headerEL1 = document.getElementById("s1");
+        headerEL1.innerHTML = stat1;
+
+        let stat2 = this.cijfertellen();
+        let headerEL2 = document.getElementById("s2");
+        headerEL2.innerHTML = stat2;
+        let stat3 = this.cijfertellen();
+        
+        let stat4 = this.cijfertellen();
+       
+        let stat5 = this.cijfertellen();
+       
+        let stat6 = this.cijfertellen();
+
+    })
+
+
+
+    console.log("hello world!");
+    let stat1 = this.cijfertellen();
+    let headerEL1 = document.getElementById("s1");
+    headerEL1.innerHTML = stat1;
+    let stat2 = this.cijfertellen();
+    
+    let stat3 = this.cijfertellen();
+    
+    let stat4 = this.cijfertellen();
+   
+    let stat5 = this.cijfertellen();
+   
+    let stat6 = this.cijfertellen();
+   
+
+    let d1 = this.rollDice();
+    
+    }
+    rollDice(){
+        return Math.floor(Math.random() * 6+1);
+    }
+
+    cijfertellen(){
+        let d1 = this.rollDice();
+        let d2 = this.rollDice();
+        let d3 = this.rollDice();
+        let d4 = this.rollDice();
+        let diceArray = [d1, d2, d3, d4];
+        diceArray.sort();
+        let som = diceArray[1] + diceArray[2] + diceArray[3]
+        return som;
+       
     }
 }
+
 let app = new App();
 app.runApplication();
 
-
-
-
-
-
+       
+    
+        
